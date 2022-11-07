570fc07bdf46d62fd56aacb4f9a19817ea217eab
NishatFariza <nishatfarzia78@gmail.com>
2022-11-07 15:29:56 +0600

    wpi:js-scope

--- scope.js
+++ scope.js
@@ -3,6 +3,27 @@ const i_am_global = 100;
 
 //local scope
 function local() {
-    const i_am_local = 50;
+    const i_am_local = 75;
     console.log(i_am_local);
+}
+
+
+//block scope
+{
+    const i_am_block = 50;
+
+    function blocked() {
+        console.log(i_am_block)
+    }
+}
+
+//lexical scope
+function lexical() {
+    const a = 20;
+    function outerInner() {
+        const b = 40;
+        function inner() {
+            console.log(a)
+        }
+    }
 }
