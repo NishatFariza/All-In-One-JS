25dc5524d5affe9e3f13f75e7676528b932fcb5b
NishatFariza <nishatfarzia78@gmail.com>
2022-11-29 12:50:42 +0600

    wip:array map

--- arrayIndexOf.js
+++ arrayIndexOf.js
@@ -1,3 +1,5 @@
 const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 
 console.log(beasts.indexOf('ant'))
+console.log(beasts.indexOf('bison'))
+console.log(beasts.indexOf('duck'))
---
+++ arrayMap.js
@@ -0,0 +1,5 @@
+const array1 = [1, 4, 9, 16];
+
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
